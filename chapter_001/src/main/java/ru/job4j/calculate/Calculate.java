c4d3afedff9d7b6a27aff2057fb88e3bd503deb7
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2018-08-08 15:55:02 +0300

    1.3. JavaDoc. Code convention[#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j.calculate;
 
+/** 
+ * Calculate.
+ *
+ * @author Sir-Hedgehog (quaresma_08@mail.ru)
+*/
+
 public class Calculate {
+	
+	/**
+	* Конструктор, вывод строки в консоль
+	* @param args - args.
+	*/
+	
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
+* @version 1
+* @since 08.08.2018
+*/
+package ru.job4j.calculate;
