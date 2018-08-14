70dae90842aca5802d0141c9cdd5efe66c2d6c23
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2018-08-14 14:56:23 +0300

    4.2. "Максимум из двух чисел.[#189]"

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,7 @@
+package ru.job4j.max;
+
+public class Max {
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Sir-Hedgehog (mailto:parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class MaxTest {
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenSecondLessFirst() {
+        Max maxim = new Max();
+        int result = maxim.max(2, 1);
+        assertThat(result, is(2));
+    }
+}
