ce0cb4458a7c0a3643820e9a2f2a4f11e1783a2a
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2018-08-22 14:31:58 +0300

    5.2. "Создать программу вычисляющую факториал [#193]"

--- chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -4,12 +4,6 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-/**
- * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
- * @version $Id$
- * @since 0.1
- */
-
 public class FactorialTest {
     @Test
     public void whenNumberMoreZero() {
