7c9bee085cbea87225359f857d864a9f782924bf
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2018-08-31 23:45:54 +0300

    "Вопросы" [#226]

--- chapter_001/src/main/java/ru/job4j/array/Sorting.java
+++ chapter_001/src/main/java/ru/job4j/array/Sorting.java
@@ -1,20 +1,28 @@
 package ru.job4j.array;
 
-import java.util.Arrays;
-
 public class Sorting {
     public int[] plus(int[] first, int[] second) {
         int[] result = new int[first.length + second.length];
-        System.arraycopy(first, 0, result, 0, first.length);
-        System.arraycopy(second, 0, result, first.length, second.length);
-        for (int out = result.length - 1; out > 0; out--) {
-            for (int in = 0; in < out; in++) {
-                if (result[in] > result[in + 1]) {
-                    int temp = result[in];
-                    result[in] = result[in + 1];
-                    result[in + 1] = temp;
-                }
+        int indexFirst = 0, indexSecond = 0, indexResult = 0;
+        while (indexFirst < first.length && indexSecond < second.length) {
+            if (first[indexFirst] < second[indexSecond]) {
+                result[indexResult] = first[indexFirst];
+                indexFirst++;
+            } else {
+                result[indexResult] = second[indexSecond];
+                indexSecond++;
             }
+            indexResult++;
+        }
+        while (indexFirst < first.length) {
+            result[indexResult] = first[indexFirst];
+            indexFirst++;
+            indexResult++;
+        }
+        while (indexSecond < second.length) {
+            result[indexResult] = second[indexSecond];
+            indexSecond++;
+            indexResult++;
         }
         return result;
     }
--- chapter_001/src/test/java/ru/job4j/array/SortingTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SortingTest.java
@@ -6,12 +6,62 @@ import static org.junit.Assert.assertThat;
 
 public class SortingTest {
     @Test
-    public void whenFirstSortArrayPlusSecondSortArray() {
+    public void whenPlusTheSameNumber() {
         Sorting sort = new Sorting();
-        int[] input1 = {0, 2, 3, 4, 8, 29};
-        int[] input2 = {1, 4, 7, 10, 13, 16};
+        int[] input1 = {0, 2, 3, 8, 29};
+        int[] input2 = {1, 7, 10, 13, 16};
         int[] result = sort.plus(input1, input2);
-        int[] expect = new int[] {0, 1, 2, 3, 4, 4, 7, 8, 10, 13, 16, 29};
+        int[] expect = new int[] {0, 1, 2, 3, 7, 8, 10, 13, 16, 29};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenPlusTheDifferentNumber() {
+        Sorting sort = new Sorting();
+        int[] input1 = {2, 3, 4};
+        int[] input2 = {1, 7, 10, 16};
+        int[] result = sort.plus(input1, input2);
+        int[] expect = new int[] {1, 2, 3, 4, 7, 10, 16};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenPlusTheDifferentNumber1() {
+        Sorting sort = new Sorting();
+        int[] input1 = {2, 3, 5, 99};
+        int[] input2 = {1, 4, 7};
+        int[] result = sort.plus(input1, input2);
+        int[] expect = new int[] {1, 2, 3, 4, 5, 7, 99};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTheFirstArrayIsEmpty() {
+        Sorting sort = new Sorting();
+        int[] input1 = {};
+        int[] input2 = {1, 4, 7, 10, 16};
+        int[] result = sort.plus(input1, input2);
+        int[] expect = new int[] {1, 4, 7, 10, 16};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTheSecondArrayIsEmpty() {
+        Sorting sort = new Sorting();
+        int[] input1 = {1, 2, 3};
+        int[] input2 = {};
+        int[] result = sort.plus(input1, input2);
+        int[] expect = new int[] {1, 2, 3};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTheBothArraysAreEmpty() {
+        Sorting sort = new Sorting();
+        int[] input1 = {};
+        int[] input2 = {};
+        int[] result = sort.plus(input1, input2);
+        int[] expect = new int[] {};
         assertThat(result, is(expect));
     }
 }
