012017f51f18c7d26752304f81a91aa67993b863
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2018-09-04 14:41:07 +0300

    "Крестики-нолики на JavaFX" [#49777]

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -8,45 +8,94 @@ public class Logic3T {
     }
 
     public boolean isWinnerX() {
-        boolean result = false;
-        if ((table[0][0].hasMarkX() && table[0][1].hasMarkX() && table[0][2].hasMarkX())
-                || (table[1][0].hasMarkX() && table[1][1].hasMarkX() && table[1][2].hasMarkX())
-                || (table[2][0].hasMarkX() && table[2][1].hasMarkX() && table[2][2].hasMarkX())
-                || (table[0][0].hasMarkX() && table[1][0].hasMarkX() && table[2][0].hasMarkX())
-                || (table[0][1].hasMarkX() && table[1][1].hasMarkX() && table[2][1].hasMarkX())
-                || (table[0][2].hasMarkX() && table[1][2].hasMarkX() && table[2][2].hasMarkX())
-                || (table[0][0].hasMarkX() && table[1][1].hasMarkX() && table[2][2].hasMarkX())
-                || (table[0][2].hasMarkX() && table[1][1].hasMarkX() && table[2][0].hasMarkX())) {
-            result = true;
+        boolean resultHorizontal = true;
+        for (int out = 0; out < table.length; out++) {
+            for (int in = 1; in < table.length; in++) {
+                if (table[out][0].hasMarkX() != table[out][in].hasMarkX()) {
+                    resultHorizontal = false;
+                    break;
+                }
+            }
         }
-        return result;
+
+        boolean resultVertical = true;
+        for (int out = 0; out < table.length; out++) {
+            for (int in = 1; in < table.length; in++) {
+                if (table[0][out].hasMarkX() != table[in][out].hasMarkX()) {
+                    resultVertical = false;
+                    break;
+                }
+            }
+        }
+
+        boolean resultDiagonalOne = true;
+        for (int line = 1; line < table.length; line++) {
+            if (table[0][0].hasMarkX() != table[line][line].hasMarkX()) {
+                resultDiagonalOne = false;
+            }
+        }
+
+        boolean resultDiagonalSecond = true;
+        for (int line = 1, column = table.length - 2; line < table.length; line++, column--) {
+            if (table[0][table.length - 1].hasMarkX() != table[line][column].hasMarkX()) {
+                resultDiagonalSecond = false;
+            }
+        }
+
+        return resultHorizontal || resultVertical || resultDiagonalOne || resultDiagonalSecond;
     }
 
     public boolean isWinnerO() {
-        boolean result = false;
-        if ((table[0][0].hasMarkO() && table[0][1].hasMarkO() && table[0][2].hasMarkO())
-                || (table[1][0].hasMarkO() && table[1][1].hasMarkO() && table[1][2].hasMarkO())
-                || (table[2][0].hasMarkO() && table[2][1].hasMarkO() && table[2][2].hasMarkO())
-                || (table[0][0].hasMarkO() && table[1][0].hasMarkO() && table[2][0].hasMarkO())
-                || (table[0][1].hasMarkO() && table[1][1].hasMarkO() && table[2][1].hasMarkO())
-                || (table[0][2].hasMarkO() && table[1][2].hasMarkO() && table[2][2].hasMarkO())
-                || (table[0][0].hasMarkO() && table[1][1].hasMarkO() && table[2][2].hasMarkO())
-                || (table[0][2].hasMarkO() && table[1][1].hasMarkO() && table[2][0].hasMarkO())) {
-            result = true;
+        boolean resultHorizontal = true;
+        for (int out = 0; out < table.length; out++) {
+            for (int in = 1; in < table.length; in++) {
+                if (table[out][0].hasMarkO() != table[out][in].hasMarkO()) {
+                    resultHorizontal = false;
+                    break;
+                }
+            }
         }
-        return result;
+
+        boolean resultVertical = true;
+        for (int out = 0; out < table.length; out++) {
+            for (int in = 1; in < table.length; in++) {
+                if (table[0][out].hasMarkO() != table[in][out].hasMarkO()) {
+                    resultVertical = false;
+                    break;
+                }
+            }
+        }
+
+        boolean resultDiagonalOne = true;
+        for (int line = 1; line < table.length; line++) {
+            if (table[0][0].hasMarkO() != table[line][line].hasMarkO()) {
+                resultDiagonalOne = false;
+            }
+        }
+
+        boolean resultDiagonalSecond = true;
+        for (int line = 1, column = table.length - 2; line < table.length; line++, column--) {
+            if (table[0][table.length - 1].hasMarkO() != table[line][column].hasMarkO()) {
+                resultDiagonalSecond = false;
+            }
+        }
+
+        return resultHorizontal || resultVertical || resultDiagonalOne || resultDiagonalSecond;
     }
 
     public boolean hasGap() {
         boolean result = false;
-            for (int out = 0; out < table.length; out++) {
-                for (int in = 0; in < table.length; in++) {
-                    if (!table[out][in].hasMarkX() && !table[out][in].hasMarkO()) {
-                        result = true;
-                        break;
-                    }
+        for (int out = 0; out < table.length; out++) {
+            for (int in = 0; in < table.length; in++) {
+                if (!table[out][in].hasMarkX() && !table[out][in].hasMarkO()) {
+                    result = true;
+                    break;
                 }
             }
+            if (result == true) {
+                break;
+            }
+        }
         return result;
     }
 }
