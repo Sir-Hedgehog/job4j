fce4dba6089ef1494707fd982c661d09e6c979da
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2018-11-29 07:04:52 +0300

    "Конвертация ArrayList в двухмерный массив" [#10035]

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -1,17 +1,24 @@
 package ru.job4j.list;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConvertList2Array {
     public int[][] toArray(List<Integer> list, int rows) {
-        int cells = (int) Math.ceil(list.size() / rows);
-        for (int in : list) {
-            if (list.size() % rows != 0) {
-                list.add(0);
+        int cells = (int) Math.ceil((float) list.size() / rows);
+        int[][] array = new int[rows][cells];
+        int index = 0;
+        for (int out = 0; out < rows; out++) {
+            for (int in = 0; in < cells; in++) {
+                if (index >= list.size()) {
+                    array[out][in] = 0;
+                } else {
+                    array[out][in] = list.get(index);
+                }
+                index++;
             }
         }
-        int[][] array = new int[rows][cells];
         return array;
     }
 }
+
--- chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -1,9 +1,12 @@
 package ru.job4j.list;
 
 import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-import java.util.Arrays;
+
 
 public class ConvertList2ArrayTest {
     @Test
@@ -16,7 +19,39 @@ public class ConvertList2ArrayTest {
         int[][] expect = {
                 {1, 2, 3},
                 {4, 5, 6},
-                {7, 0 ,0}
+                {7, 0, 0}
+        };
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when8ElementsThen10() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8),5
+
+        );
+        int[][] expect = {
+                {1, 2},
+                {3, 4},
+                {5, 6},
+                {7, 8},
+                {0, 0}
+        };
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when5ElementsThen6() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(
+                Arrays.asList(1, 2, 3, 4, 5),
+                3
+        );
+        int[][] expect = {
+                {1, 2},
+                {3, 4},
+                {5, 0}
         };
         assertThat(result, is(expect));
     }
