c54ef80cbc6131312517a1efcae68536ec71c85c
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2019-01-30 15:08:07 +0300

    "Подсчет функции в диапазоне" [#24251]

---
+++ chapter_004/src/main/java/ru/job4j/lambda/Diapason.java
@@ -0,0 +1,21 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
+ * @version $Id$
+ * @since 30.01.2019
+ */
+
+class Diapason {
+    List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List <Double> list = new ArrayList<>();
+        for (double index = start; index!= end; index++) {
+            list.add(func.apply(index));
+        }
+        return list;
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/lambda/DiapasonTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class DiapasonTest {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        Diapason function = new Diapason();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSquareFunctionThenSquareResults() {
+        Diapason function = new Diapason();
+        List<Double> result = function.diapason(4, 7, x -> x * x);
+        List<Double> expected = Arrays.asList(16D, 25D, 36D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenLogFunctionThenLogResults() {
+        Diapason function = new Diapason();
+        List<Double> result = function.diapason(2, 5, x -> Math.log10(x));
+        List<Double> expected = Arrays.asList(0.3010299956639812D, 0.47712125471966244D, 0.6020599913279624D);
+        assertThat(result, is(expected));
+    }
+
+}
