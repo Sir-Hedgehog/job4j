fca338a09b259af5513959b28052cc739d30d2ba
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2019-01-31 18:28:07 +0300

    "Подсчет функции в диапазоне" [#24251]

--- chapter_004/src/main/java/ru/job4j/lambda/Diapason.java
+++ chapter_004/src/main/java/ru/job4j/lambda/Diapason.java
@@ -7,13 +7,13 @@ import java.util.function.Function;
 /**
  * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
  * @version $Id$
- * @since 30.01.2019
+ * @since 31.01.2019
  */
 
 class Diapason {
     List<Double> diapason(int start, int end, Function<Double, Double> func) {
-        List <Double> list = new ArrayList<>();
-        for (double index = start; index!= end; index++) {
+        List<Double> list = new ArrayList<>();
+        for (double index = start; index != end; index++) {
             list.add(func.apply(index));
         }
         return list;
