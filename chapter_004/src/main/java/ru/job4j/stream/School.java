df9c5a6f66644e709d0c4409e48abecf2fb82124
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2019-02-03 12:58:06 +0300

    "Фильтрация учеников" [#110058]

--- chapter_004/src/main/java/ru/job4j/stream/School.java
+++ chapter_004/src/main/java/ru/job4j/stream/School.java
@@ -7,19 +7,11 @@ import java.util.stream.Collectors;
 /**
  * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
  * @version $Id$
- * @since 02.02.2019
+ * @since 03.02.2019
  */
 
 class School {
-    List<Student> collect10A(List<Student> students, Predicate<Student> predicate) {
-        return students.stream().filter(predicate -> 100 >= predicate.getScore() && predicate.getScore() >= 70).collect(Collectors.toList());
-    }
-
-    List<Student> collect10B(List<Student> students, Predicate<Student> predicate) {
-        return students.stream().filter(predicate -> 70 > predicate.getScore() && predicate.getScore() >= 50).collect(Collectors.toList());
-    }
-
-    List<Student> collect10C(List<Student> students, Predicate<Student> predicate) {
-        return students.stream().filter(predicate -> 50 > predicate.getScore() && predicate.getScore() >= 0).collect(Collectors.toList());
+    List<Student> collect(List<Student> students, Predicate<Student> predicate){
+        return students.stream().filter(predicate).collect(Collectors.toList());
     }
 }
--- chapter_004/src/test/java/ru/job4j/stream/SchoolTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/SchoolTest.java
@@ -13,10 +13,10 @@ public class SchoolTest {
     public void whenCollect10AThen() {
         School school = new School();
         List<Student> list = new ArrayList<>();
-        list.add(new  Student(76));
+        list.add(new Student(76));
         list.add(new Student(98));
         list.add(new Student(50));
-        List<Student> result = school.collect10A(list, predicate  -> true);
+        List<Student> result = school.collect(list, predicate  -> predicate.getScore() >= 70 && predicate.getScore() <= 100);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(76));
         expected.add(new Student(98));
@@ -27,10 +27,10 @@ public class SchoolTest {
     public void whenCollect10BThen() {
         School school = new School();
         List<Student> list = new ArrayList<>();
-        list.add(new  Student(15));
+        list.add(new Student(15));
         list.add(new Student(56));
         list.add(new Student(65));
-        List<Student> result = school.collect10B(list, predicate -> true);
+        List<Student> result = school.collect(list, predicate -> predicate.getScore() < 70 && predicate.getScore() >= 50);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(56));
         expected.add(new Student(65));
@@ -41,10 +41,10 @@ public class SchoolTest {
     public void whenCollect10CThen() {
         School school = new School();
         List<Student> list = new ArrayList<>();
-        list.add(new  Student(15));
+        list.add(new Student(15));
         list.add(new Student(56));
         list.add(new Student(5));
-        List<Student> result = school.collect10C(list, predicate -> true);
+        List<Student> result = school.collect(list, predicate -> predicate.getScore() >= 0 && predicate.getScore() < 50);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(15));
         expected.add(new Student(5));
