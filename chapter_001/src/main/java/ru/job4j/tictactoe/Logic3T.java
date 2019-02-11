028d5048d737f1220d0b77bed9cb7dfc508fe675
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2019-02-11 18:15:44 +0300

    "Крестики-нолики на JavaFX" [#49777]

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -30,25 +30,25 @@ class Logic3T {
     }
 
     boolean isWinnerX() {
-        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1) ||
-                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1) ||
-                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkX, 0, this.table.length - 1, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0);
+        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0)
+                || this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
+                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1)
+                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkX, 0, this.table.length - 1, 1, 0)
+                || this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0);
     }
 
     boolean isWinnerO() {
-        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1) ||
-                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1) ||
-                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkO, 0, this.table.length - 1, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0);
+        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0)
+                || this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1)
+                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1)
+                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkO, 0, this.table.length - 1, 1, 0)
+                || this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0);
     }
 
     boolean hasGap() {
