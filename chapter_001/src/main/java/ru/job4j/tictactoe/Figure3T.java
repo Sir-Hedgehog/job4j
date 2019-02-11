7353d0f7c8d27771f406b9b7d9c6f271a6c7574c
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2019-02-11 18:10:41 +0300

    "Крестики-нолики на JavaFX" [#49777]

--- chapter_001/src/main/java/ru/job4j/tictactoe/Figure3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Figure3T.java
@@ -2,28 +2,28 @@ package ru.job4j.tictactoe;
 
 import javafx.scene.shape.Rectangle;
 
-public class Figure3T extends Rectangle {
+class Figure3T extends Rectangle {
     private boolean markX = false;
     private boolean markO = false;
 
-    public Figure3T() {
+    Figure3T() {
     }
 
-    public Figure3T(boolean markX) {
+    Figure3T(boolean markX) {
         this.markX = markX;
         this.markO = !markX;
     }
 
-    public void take(boolean markX) {
+    void take(boolean markX) {
         this.markX = markX;
         this.markO = !markX;
     }
 
-    public boolean hasMarkX() {
+    boolean hasMarkX() {
         return this.markX;
     }
 
-    public boolean hasMarkO() {
+    boolean hasMarkO() {
         return this.markO;
     }
 }
--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -1,78 +1,57 @@
 package ru.job4j.tictactoe;
 
-public class Logic3T {
+import java.util.function.Predicate;
+
+/**
+ * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
+ * @version $Id$
+ * @since 11.02.2019
+ */
+
+class Logic3T {
     private final Figure3T[][] table;
 
-    public Logic3T(Figure3T[][] table) {
+    Logic3T(Figure3T[][] table) {
         this.table = table;
     }
 
-    public boolean isWinnerX() {
-        boolean result = false;
-        int horizon;
-        int vertical;
-        int diagonal1 = 0;
-        int diagonal2 = 0;
-
-        for (int out = 0, column = table.length - 1; out < table.length; out++, column--) {
-            horizon = 0;
-            vertical = 0;
-            if (table[out][out].hasMarkX()) {
-                diagonal1++;
-            }
-            if (table[out][column].hasMarkX()) {
-                diagonal2++;
-            }
-            for (int in = 0; in < table.length; in++) {
-                if (table[out][in].hasMarkX()) {
-                    horizon++;
-                }
-                if (table[in][out].hasMarkX()) {
-                    vertical++;
-                }
-            }
-            if (horizon == table.length || vertical == table.length || diagonal1 == table.length || diagonal2 == table.length) {
-                result = true;
+    private boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
+        boolean result = true;
+        for (int index = 0; index != this.table.length; index++) {
+            Figure3T cell = this.table[startX][startY];
+            startX += deltaX;
+            startY += deltaY;
+            if (!predicate.test(cell)) {
+                result = false;
                 break;
             }
         }
         return result;
     }
 
-
-    public boolean isWinnerO() {
-        boolean result = false;
-        int horizon;
-        int vertical;
-        int diagonal1 = 0;
-        int diagonal2 = 0;
-
-        for (int out = 0, column = table.length - 1; out < table.length; out++, column--) {
-            horizon = 0;
-            vertical = 0;
-            if (table[out][out].hasMarkO()) {
-                diagonal1++;
-            }
-            if (table[out][column].hasMarkO()) {
-                diagonal2++;
-            }
-            for (int in = 0; in < table.length; in++) {
-                if (table[out][in].hasMarkO()) {
-                    horizon++;
-                }
-                if (table[in][out].hasMarkO()) {
-                    vertical++;
-                }
-            }
-            if (horizon == table.length || vertical == table.length || diagonal1 == table.length || diagonal2 == table.length) {
-                result = true;
-                break;
-            }
-        }
-        return result;
+    boolean isWinnerX() {
+        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0) ||
+                this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1) ||
+                this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1) ||
+                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1) ||
+                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, 0, 1) ||
+                this.fillBy(Figure3T::hasMarkX, 0, this.table.length - 1, 1, 0) ||
+                this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1) ||
+                this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0);
     }
 
-    public boolean hasGap() {
+    boolean isWinnerO() {
+        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0) ||
+                this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1) ||
+                this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1) ||
+                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1) ||
+                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, 0, 1) ||
+                this.fillBy(Figure3T::hasMarkO, 0, this.table.length - 1, 1, 0) ||
+                this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1) ||
+                this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0);
+    }
+
+    boolean hasGap() {
         boolean result = false;
         for (int out = 0; out < table.length; out++) {
             for (int in = 0; in < table.length; in++) {
