33d4f55a5be88357736af33bc237b2283796f716
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2019-02-18 00:43:14 +0300

    "Итератор для двухмерного массива int[][]" [#9539]

---
+++ chapter_006/src/main/java/ru/job4j/iterator/IteratorFor2Matrix.java
@@ -0,0 +1,33 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+
+/**
+ * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
+ * @version $Id$
+ * @since 18.02.2019
+ */
+
+class IteratorFor2Matrix implements Iterator {
+    private final int[][] matrix;
+    private int in = 0;
+    private int out = 0;
+
+    IteratorFor2Matrix(final int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return matrix.length - 1 > out;
+    }
+
+    @Override
+    public Integer next() {
+        if (matrix[out].length == in) {
+            out++;
+            in = 0;
+        }
+        return matrix[out][in++];
+    }
+}
---
+++ chapter_006/src/test/java/ru/job4j/iterator/IteratorFor2MatrixTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.iterator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class IteratorFor2MatrixTest {
+    private Iterator<Integer> it;
+
+    @Before
+    public void setUp() {
+        it = new IteratorFor2Matrix(new int[][]{{1}, {3, 4}, {7}});
+    }
+
+    @Test
+    public void testsThatNextMethodDoesNotDependsOnPriorHasNextInvocation() {
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(7));
+    }
+
+    @Test
+    public void sequentialHasNextInvocationDoesNotAffectRetrievalOrder() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(7));
+    }
+
+    @Test
+    public void hasNextNextSequentialInvocation() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(7));
+        assertThat(it.hasNext(), is(false));
+    }
+}
