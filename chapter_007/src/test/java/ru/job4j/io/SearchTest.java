3f55cbf0d04b44a564ce931b40b24595a868e559
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2019-04-02 20:42:37 +0300

    "Сканирование файловой системы" [#106929]

--- chapter_007/src/test/java/ru/job4j/io/SearchTest.java
+++ chapter_007/src/test/java/ru/job4j/io/SearchTest.java
@@ -8,27 +8,27 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
  * @version $Id$
- * @since 01.04.2019
+ * @since 02.04.2019
  */
 
 public class SearchTest {
     @Test
     public void whenSearchNecessaryExpansionsThenShowResult() throws IOException {
+        final String divider = File.separator;
         Search search = new Search();
         List<String> list = new ArrayList<>();
         list.add("txt");
         String tmpDir = System.getProperty("java.io.tmpdir");
         File basicDir = new File(tmpDir + "Basic");
         basicDir.mkdir();
-        File currentDir1 = new File(basicDir.getPath() + "\\CurrentDir1");
+        File currentDir1 = new File(basicDir.getPath() + divider + "CurrentDir1");
         currentDir1.mkdir();
-        File currentDir2 = new File(basicDir.getPath() + "\\CurrentDir2");
+        File currentDir2 = new File(basicDir.getPath() + divider + "\\CurrentDir2");
         currentDir2.mkdir();
         File file1 = new File(basicDir, "File1.txt");
         File file2 = new File(currentDir1, "File2.xlsx");
@@ -41,15 +41,9 @@ public class SearchTest {
         file4.createNewFile();
         file5.createNewFile();
         List<File> result = search.list(basicDir.getPath(), list);
-        List<File> expect = new ArrayList<>();
-        File expectFile1 = new File("C:\\Users\\18B8~1\\AppData\\Local\\Temp\\Basic\\CurrentDir1\\File3.txt");
-        File expectFile2 = new File("C:\\Users\\18B8~1\\AppData\\Local\\Temp\\Basic\\CurrentDir2\\File4.txt");
-        File expectFile3 = new File("C:\\Users\\18B8~1\\AppData\\Local\\Temp\\Basic\\File1.txt");
-        expect.add(expectFile1);
-        expect.add(expectFile2);
-        expect.add(expectFile3);
+        List<File> expect = List.of(file1, file4, file3);
         Collections.sort(result);
-        assertThat(result, is(expect));
+        assertTrue(result.containsAll(expect));
     }
 }
 
