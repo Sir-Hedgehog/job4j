57b8ab014cf50be50da69df27c87372cba3a9db9
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2019-04-14 14:24:02 +0300

    "Архивировать проект" [#861]

--- chapter_007/src/main/java/ru/job4j/io/PackZip.java
+++ chapter_007/src/main/java/ru/job4j/io/PackZip.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -9,10 +8,14 @@ import java.util.zip.ZipOutputStream;
 /**
  * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
  * @version $Id$
- * @since 13.04.2019
+ * @since 14.04.2019
  */
 
 public class PackZip {
+    private byte[] buffer = new byte[4096];
+    private Search search = new Search();
+
+
     public void achieve(Args args) throws IOException {
         String zip = args.output();
         try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zip))) {
@@ -25,49 +28,17 @@ public class PackZip {
     }
 
     private void form(ZipOutputStream zos, File source, List<String> expansions) throws IOException {
-        byte[] buffer = new byte[4096];
-        File[] files = source.listFiles();
-        List<String> listOfNames = new ArrayList<>();
-        List<String> listOfFiles = new ArrayList<>();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    form(zos, file, expansions);
-                    continue;
+        List<File> list = search.listOfOthers(source.getName(), expansions);
+        for (File file : list) {
+            zos.putNextEntry(new ZipEntry(file.getAbsolutePath().substring(file.getName().length())));
+            try (FileInputStream fis = new FileInputStream(file)) {
+                while (fis.read(buffer) != -1) {
+                    zos.write(buffer);
                 }
-                if (exclude(file, expansions)) {
-                    listOfNames.add(file.getName());
-                    listOfFiles.add(file.getPath());
-                    String[] temp = {};
-                    String[] filesToZip = listOfFiles.toArray(temp);
-                    String[] namesToZip = listOfNames.toArray(temp);
-                    for (int index = 0; index < filesToZip.length; index++) {
-                        zos.putNextEntry(new ZipEntry(namesToZip[index]));
-                        try (FileInputStream fis = new FileInputStream(filesToZip[index])) {
-                            while (fis.read(buffer) != -1) {
-                                zos.write(buffer);
-                            }
-                            zos.closeEntry();
-                        } catch (FileNotFoundException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
+                zos.closeEntry();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
         }
-    }
-
-    private boolean exclude(File file, List<String> expansions) {
-        Search search = new Search();
-        boolean result = true;
-        if (expansions != null) {
-            while (true) {
-                if (search.list(file.getPath(), expansions) != null) {
-                    result = false;
-                    break;
-                }
-            }
-        }
-        return result;
     }
 }
--- chapter_007/src/main/java/ru/job4j/io/Search.java
+++ chapter_007/src/main/java/ru/job4j/io/Search.java
@@ -35,6 +35,31 @@ public class Search {
         return result;
     }
 
+    List<File> listOfOthers(String parent, List<String> expansions) {
+        List<File> result = new ArrayList<>();
+        Queue<File> data = new LinkedList<>();
+        File file = new File(parent);
+        data.offer(file);
+        while (!data.isEmpty()) {
+            File element = data.poll();
+            if (!element.isDirectory()) {
+                String fileName = element.getName();
+                String extension = this.filter(fileName);
+                if (!expansions.contains(extension)) {
+                    result.add(element);
+                }
+            } else {
+                File[] files = element.listFiles();
+                if (files != null) {
+                    for (File child : files) {
+                        data.offer(child);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
     private String filter(String name) {
         String extension = "";
         int index = name.lastIndexOf('.');
