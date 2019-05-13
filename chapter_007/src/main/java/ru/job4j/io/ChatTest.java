63a1d3e7de2a36aea35a49b8de0d196c94c9be20
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2019-05-13 19:33:20 +0300

    "Создать программу 'Консольный чат'" [#862]

--- chapter_007/src/main/java/ru/job4j/io/ChatTest.java
+++ chapter_007/src/main/java/ru/job4j/io/ChatTest.java
@@ -13,28 +13,20 @@ public class ChatTest {
     public static void main(String[] args) throws IOException {
         ConsoleChat chat = new ConsoleChat();
         InputStream in = ConsoleChat.class.getResourceAsStream("/powerfulRandom.txt");
-
         List<String> list = chat.discuss(in);
-        System.out.println(list);
-
-
-        //BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        //File file = new File("C:\\projects\\job4j\\powerfulRandom.txt");
-        //System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream("stdout.log")), true));
-
-        /*final String divider = File.separator;
+        final String divider = File.separator;
         String tmpDir = System.getProperty("java.io.tmpdir");
         File chatDir = new File(tmpDir + divider + "Chat");
         chatDir.mkdir();
         File test = new File(chatDir, "Char.txt");
         test.createNewFile();
-        try (Writer writer = new OutputStreamWriter(new FileOutputStream(chatDir), "utf-8")) {
+        try (Writer writer = new OutputStreamWriter(new FileOutputStream(test))) {
             for (String str : list) {
-                writer.write(str);
+                writer.write(str + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
     }
 }
 
