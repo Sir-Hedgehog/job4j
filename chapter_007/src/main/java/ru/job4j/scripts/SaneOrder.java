ec516aebfb7c13851d8b9bb52b5dd29fd89dd961
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2019-09-23 23:26:43 +0300

    "DB Scripts" [#138867]

--- chapter_007/src/main/java/ru/job4j/scripts/SaneOrder.java
+++ chapter_007/src/main/java/ru/job4j/scripts/SaneOrder.java
@@ -5,33 +5,40 @@ import java.util.*;
 /**
  * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
  * @version $Id$
- * @since 08.09.2019
+ * @since 23.09.2019
  */
 
-public class SaneOrder  {
-    private List<VulnerabilityScript> result = new ArrayList<>();
+public class SaneOrder {
+    private VulnerabilityScript root;
 
-    public String order(List<VulnerabilityScript> list) {
-        for (int index = 0; index < list.size(); index++) {
-            if (list.get(index).getDependencies().isEmpty()) {
-                result.add(0, list.get(index));
-                list.remove(index);
-                index--;
-            }
-        }
-        while (!list.isEmpty()) {
-            for (int out = 0; out < result.size(); out++) {
-                for (int in = 0; in < list.size(); in++) {
-                    int current = result.get(out).getScriptId();
-                    list.get(in).getDependencies().removeIf(x -> current == x);
-                    if (list.get(in).getDependencies().isEmpty()) {
-                        result.add(0, list.get(in));
-                        list.remove(in);
-                        break;
-                    }
+    public SaneOrder(VulnerabilityScript root) {
+        this.root = root;
+    }
+
+    /**
+     * Метод выстраивает отсортированную очередь (используется алгоритм обхода в ширину)
+     * @param unsorted - неотсортированный список скриптов
+     * @return - отсортированный список скриптов
+     */
+    public Set<Integer> order(List<VulnerabilityScript> unsorted) {
+        Deque<VulnerabilityScript> deque = new LinkedList<>();
+        Set<Integer> result = new LinkedHashSet<>();
+        result.add(this.root.getScriptId());
+        deque.add(this.root);
+        while (!unsorted.isEmpty()) {
+            VulnerabilityScript script = deque.pollFirst();
+            for (int index = 0; index < unsorted.size(); index++) {
+                if (!Objects.requireNonNull(script).getDependencies().isEmpty()
+                        && script.getDependencies().contains(unsorted.get(index).getScriptId())) {
+                    result.add(unsorted.get(index).getScriptId());
+                    deque.add(unsorted.get(index));
+                    unsorted.remove(index);
+                    index--;
                 }
             }
         }
-        return result.toString();
+        return result;
     }
 }
+
+
