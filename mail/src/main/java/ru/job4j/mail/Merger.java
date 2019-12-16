ebed0a48747b5372482a37c503112a0609ae1a03
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2019-12-16 22:42:08 +0300

    "Почта" [#184458]

--- mail/src/main/java/ru/job4j/mail/Merger.java
+++ mail/src/main/java/ru/job4j/mail/Merger.java
@@ -1,20 +1,14 @@
 package ru.job4j.mail;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
- * @version 2.0
- * @since 14.11.2019
+ * @version 3.0
+ * @since 16.12.2019
  */
 
 public class Merger {
-    private String mail;
-
-    public Merger(String mail) {
-        this.mail = mail;
-    }
 
     /**
      * Метод объединяет почты одного и того же пользователя
@@ -22,19 +16,133 @@ public class Merger {
      * @return - отсортированный список почтовых адресов
      */
 
-    public Map<String, LinkedHashSet<String>> merge(ConcurrentHashMap<String, LinkedHashSet<String>> mapOfMails) {
-        Map<String, LinkedHashSet<String>> result = new HashMap<>();
-        LinkedHashSet<String> deque = new LinkedHashSet<>();
+    public Map<String, Set<String>> merge(TreeMap<String, Set<String>> mapOfMails) {
+        Map<String, Set<String>> result = new HashMap<>();
+        Map<String, String> backward = new HashMap<>();
+        for (Map.Entry<String, Set<String>> mails : mapOfMails.entrySet()) {
+            String name = null;
+            for (String element : mails.getValue()) {
+                if (backward.containsKey(element)) {
+                    name = backward.get(element);
+                    break;
+                }
+            }
+            if (name != null) {
+                result.get(name).addAll(mails.getValue());
+                for (String current : mails.getValue()) {
+                    backward.put(current, name);
+                }
+            } else {
+                name = mails.getKey();
+                result.put(name, new HashSet<>());
+                result.get(name).addAll(mails.getValue());
+                for (String gr : mails.getValue()) {
+                    backward.put(gr, name);
+                }
+            }
+        }
+        return result;
+    }
+
+
+
+
+
+
+
+
+
+        //ConcurrentHashMap<String, Set<String>> result = new ConcurrentHashMap<>();
+        //Deque<String> deque = new LinkedList<>();
+        //Set<String> deque = new LinkedHashSet<>();
+        //String key = "";
+        /*ConcurrentHashMap<String, LinkedHashSet<String>> result = new ConcurrentHashMap<>();
+        //Deque<String> deque = new LinkedList<>();
+        Set<String> deque = new LinkedHashSet<>();
+        List<String> list = new ArrayList<>();
         String key = "";
+
+        //LinkedHashSet<String> agent = new LinkedHashSet<>();
+        //List<String> listOfAgents = new ArrayList<>();
+        //List<String> listOfAbsent = new ArrayList<>();
+        //List<String> list2 = new ArrayList<>(new LinkedHashSet<>(list1));
         deque.add(this.mail);
-        for (Map.Entry<String, LinkedHashSet<String>> mails : mapOfMails.entrySet()) {
-            for (String mail : mails.getValue()) {
+        for (Map.Entry<String, LinkedHashSet<String>> users : mapOfMails.entrySet()) {
+            for (String mail : users.getValue()) {
                 if (!mail.contains("@")) {
                     return null;
                 }
             }
         }
+        for (Map.Entry<String, LinkedHashSet<String>> mails : mapOfUsers.entrySet()) {
+            if (!mails.getKey().contains("@")) {
+                return null;
+            }
+        }
         while (!mapOfMails.isEmpty()) {
+            String current = deque.iterator().next();
+            deque.remove(current);
+            LinkedHashSet<String> intermediary = new LinkedHashSet<>();
+            LinkedHashSet<String> values = new LinkedHashSet<>();
+            for (Map.Entry<String, LinkedHashSet<String>> users : mapOfUsers.entrySet()) {
+                if (users.getKey().equals(current)) {
+                    intermediary.addAll(users.getValue());
+                } else {
+                    list.addAll(users.getValue());
+                }
+            }
+            for (String elements : list) {
+                if (intermediary.contains(elements)) {
+
+                }
+            }
+            for (Map.Entry<String, LinkedHashSet<String>> mails : mapOfMails.entrySet()) {
+                if (intermediary.contains(mails.getKey())) {
+                    values.addAll(mails.getValue());
+                    key = mails.getKey();
+                    mapOfMails.remove(mails.getKey());
+                } else {
+                    deque.addAll(mails.getValue());
+                }
+            }
+            result.put(key, values);
+        }*/
+
+        /*for (LinkedHashSet<String> users : mapOfUsers.values()) {
+            //String current = deque.pollFirst();
+            String current = deque.iterator().next();
+            deque.remove(current);
+            LinkedHashSet<String> intermediary = new LinkedHashSet<>();
+            for (Map.Entry<String, LinkedHashSet<String>> mails : mapOfMails.entrySet()) {
+                if (mails.getValue().contains(current) && users.contains(mails.getKey())) {
+                    intermediary.addAll(mails.getValue());
+                    key = mails.getKey();
+                    mapOfMails.remove(key);
+                } else {
+                    deque.addAll(mails.getValue());
+                }
+            }
+            if (result.isEmpty()) {
+                result.put(key, intermediary);
+            }
+            for (Map.Entry<String, LinkedHashSet<String>> point : result.entrySet()) {
+                if (point.getValue().contains(current)) {
+                    intermediary.addAll(point.getValue());
+                    result.put(point.getKey(), intermediary);
+                    key = point.getKey();
+                    break;
+                }
+            }
+
+            result.put(key, intermediary);
+            if (mapOfMails.isEmpty()) {
+                break;
+            }
+        }*/
+        //return result;
+
+        /*while (!mapOfMails.isEmpty()) {
+            //String current = deque.pollFirst();
             String current = deque.iterator().next();
             deque.remove(current);
             LinkedHashSet<String> intermediary = new LinkedHashSet<>();
@@ -47,8 +155,19 @@ public class Merger {
                     deque.addAll(mails.getValue());
                 }
             }
+            if (result.isEmpty()) {
+                result.put(key, intermediary);
+            }
+            for (Map.Entry<String, LinkedHashSet<String>> point : result.entrySet()) {
+                if (point.getValue().contains(current)) {
+                    intermediary.addAll(point.getValue());
+                    result.put(point.getKey(), intermediary);
+                    key = point.getKey();
+                    break;
+                }
+            }
             result.put(key, intermediary);
         }
-        return result;
-    }
+        return result;*/
+
 }
--- mail/src/test/java/ru/job4j/mail/MergerTest.java
+++ mail/src/test/java/ru/job4j/mail/MergerTest.java
@@ -2,52 +2,49 @@ package ru.job4j.mail;
 
 import org.junit.Test;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 /**
  * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
- * @version 2.0
- * @since 14.11.2019
+ * @version 3.0
+ * @since 16.12.2019
  */
 
 public class MergerTest {
     @Test
     public void checkMergersOfTheFirstLevel() {
-        Merger merger = new Merger("1@");
-        //mapOfMails - неотсортированная картотека почтовых адресов
-        ConcurrentHashMap<String, LinkedHashSet<String>> mapOfMails = new ConcurrentHashMap<>();
-        LinkedHashSet<String> mails1 = new LinkedHashSet<>(Set.of("1@", "2@"));
-        LinkedHashSet<String> mails2 = new LinkedHashSet<>(Set.of("1@"));
-        LinkedHashSet<String> mails3 = new LinkedHashSet<>(Set.of("3@"));
-        LinkedHashSet<String> mails4 = new LinkedHashSet<>(Set.of("4@", "1@"));
-        mapOfMails.put("us1", mails1);
-        mapOfMails.put("us2", mails2);
-        mapOfMails.put("us3", mails3);
-        mapOfMails.put("us4", mails4);
-        Map<String, LinkedHashSet<String>> result = merger.merge(mapOfMails);
-        Map<String, LinkedHashSet<String>> expect = new HashMap<>(Map.of(
-                "us4", new LinkedHashSet<>(Set.of("1@", "2@", "4@")),
+        Merger merger = new Merger();
+        TreeMap<String, Set<String>> mapOfMails = new TreeMap<>();
+        Set<String> user1 = new LinkedHashSet<>(Set.of("1@", "2@"));
+        Set<String> user2 = new LinkedHashSet<>(Set.of("1@"));
+        Set<String> user3 = new LinkedHashSet<>(Set.of("3@"));
+        Set<String> user4 = new LinkedHashSet<>(Set.of("4@", "1@"));
+        mapOfMails.put("us1", user1);
+        mapOfMails.put("us2", user2);
+        mapOfMails.put("us3", user3);
+        mapOfMails.put("us4", user4);
+        Map<String, Set<String>> result = merger.merge(mapOfMails);
+        Map<String, Set<String>> expect = new HashMap<>(Map.of(
+                "us1", new LinkedHashSet<>(Set.of("1@", "2@", "4@")),
                 "us3", new LinkedHashSet<>(Set.of("3@"))));
-        System.out.println(result);
         assertThat(result, is(expect));
     }
 
-    /*@Test
+    @Test
     public void checkMergersOfTheSecondLevel() {
-        Merger merger = new Merger("1@");
-        ConcurrentHashMap<String, LinkedHashSet<String>> mapOfMails = new ConcurrentHashMap<>();
-        LinkedHashSet<String> mails1 = new LinkedHashSet<>(Set.of("1@", "2@"));
-        LinkedHashSet<String> mails2 = new LinkedHashSet<>(Set.of("5@", "6@"));
-        LinkedHashSet<String> mails3 = new LinkedHashSet<>(Set.of("6@", "7@"));
-        LinkedHashSet<String> mails4 = new LinkedHashSet<>(Set.of("7@"));
-        mapOfMails.put("us1", mails1);
-        mapOfMails.put("us2", mails2);
-        mapOfMails.put("us3", mails3);
-        mapOfMails.put("us4", mails4);
-        Map<String, LinkedHashSet<String>> result = merger.merge(mapOfMails);
-        Map<String, LinkedHashSet<String>> expect = new HashMap<>(Map.of(
+        Merger merger = new Merger();
+        TreeMap<String, Set<String>> mapOfMails = new TreeMap<>();
+        Set<String> user1 = new LinkedHashSet<>(Set.of("1@", "2@"));
+        Set<String> user2 = new LinkedHashSet<>(Set.of("5@", "6@"));
+        Set<String> user3 = new LinkedHashSet<>(Set.of("6@", "7@"));
+        Set<String> user4 = new LinkedHashSet<>(Set.of("7@"));
+        mapOfMails.put("us1", user1);
+        mapOfMails.put("us2", user2);
+        mapOfMails.put("us3", user3);
+        mapOfMails.put("us4", user4);
+        Map<String, Set<String>> result = merger.merge(mapOfMails);
+        Map<String, Set<String>> expect = new HashMap<>(Map.of(
                 "us1", new LinkedHashSet<>(Set.of("1@", "2@")),
                 "us2", new LinkedHashSet<>(Set.of("5@", "6@", "7@"))));
         assertThat(result, is(expect));
@@ -55,19 +52,19 @@ public class MergerTest {
 
     @Test
     public void checkMergersOfTheThirdLevel() {
-        Merger merger = new Merger("1@");
-        ConcurrentHashMap<String, LinkedHashSet<String>> mapOfMails = new ConcurrentHashMap<>();
-        LinkedHashSet<String> mails1 = new LinkedHashSet<>(Set.of("1@", "2@"));
-        LinkedHashSet<String> mails2 = new LinkedHashSet<>(Set.of("2@", "3@"));
-        LinkedHashSet<String> mails3 = new LinkedHashSet<>(Set.of("4@", "3@"));
-        LinkedHashSet<String> mails4 = new LinkedHashSet<>(Set.of("4@", "1@"));
+        Merger merger = new Merger();
+        TreeMap<String, Set<String>> mapOfMails = new TreeMap<>();
+        Set<String> mails1 = new LinkedHashSet<>(Set.of("1@", "2@"));
+        Set<String> mails2 = new LinkedHashSet<>(Set.of("2@", "3@"));
+        Set<String> mails3 = new LinkedHashSet<>(Set.of("3@", "4@"));
+        Set<String> mails4 = new LinkedHashSet<>(Set.of("4@", "1@"));
         mapOfMails.put("us1", mails1);
         mapOfMails.put("us2", mails2);
         mapOfMails.put("us3", mails3);
         mapOfMails.put("us4", mails4);
-        Map<String, LinkedHashSet<String>> result = merger.merge(mapOfMails);
+        Map<String, Set<String>> result = merger.merge(mapOfMails);
         Map<String, LinkedHashSet<String>> expect = new HashMap<>(Map.of(
-                "us4", new LinkedHashSet<>(Set.of("1@", "2@", "3@", "4@", "5@"))));
+                "us1", new LinkedHashSet<>(Set.of("1@", "2@", "3@", "4@"))));
         assertThat(result, is(expect));
-    }*/
+    }
 }
